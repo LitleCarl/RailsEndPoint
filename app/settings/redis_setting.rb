1b9a766daec2f503092d236416799fcfdd4ace22
Apple <apple@Mac-mini.local>
2016-09-13 13:48:32 +0800

    Redis密码用setting配置

---
+++ app/settings/redis_setting.rb
@@ -0,0 +1,6 @@
+# encoding: UTF-8
+
+class RedisSetting < Settingslogic
+  source "#{Rails.root}/config/redis.yml"
+  namespace Rails.env
+end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,14 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store :cookie_store, key: '_rails_end_point_session'
 
-Rails.application.config.session_store :redis_store, servers: { host: "localhost",
-                                                                port: 6379,
-                                                                db: 0,
-                                                                password: 'Fy958e5mmyb7Ta4H',
-                                                                namespace: "session"}, expires_in: 300.minutes
+redis_config = { host: RedisSetting.host,
+                 port: RedisSetting.port,
+                 db: RedisSetting.db,
+                 namespace: "session"}
+
+redis_config[:password] = RedisSetting.password if RedisSetting.password.present?
+
+puts "--------------:redis_config.password #{redis_config}:----------------"
+
+Rails.application.config.session_store :redis_store, servers: redis_config, expires_in: 300.minutes
 
